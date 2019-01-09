bd4c18e8473491b55ebcf8ac3f98c2aced4e54a0
ktoyod <ytdh7834@gmail.com>
2019-01-09 21:22:11 +0900

    fix typo and modify database.py for migration

--- flask_sample/config.py
+++ flask_sample/config.py
@@ -8,7 +8,7 @@ class DevelopmentConfig:
     DEBUG = True
 
     # SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = 'mysql+mypysql://{user}:{password}@{host}/flask_sample?charset=utf8'.format(**{
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/flask_sample?charset=utf8'.format(**{
         'user': os.getenv('DB_USER', 'root'),
         'password': os.getenv('DB_PASSWORD', ''),
         'host': os.getenv('DB_HOST', 'localhost'),
